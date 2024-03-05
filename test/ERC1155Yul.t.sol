a0ae248843b3eaacb71d8f0f38063d78c9e168e4
amatureApe <amatureApe@gmail.com>
2024-03-05 18:59:17 +0800

    more test setup

--- test/ERC1155Yul.t.sol
+++ test/ERC1155Yul.t.sol
@@ -6,13 +6,37 @@ import "deploy-yul/YulDeployer.sol";
 
 contract MyContractTest is Test {
     YulDeployer yulDeployer = new YulDeployer();
-    address public erc1155YulContract;
+    address public token;
+    address defaultSender = address(0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
+    address user1 = address(0x1);
+    address user2 = address(0x2);
+    uint256 tokenId1 = 1;
+    uint256 tokenId2 = 2;
 
     function setUp() public {
-        erc1155YulContract = yulDeployer.deployContract("ERC1155Yul");
+        token = yulDeployer.deployContract("ERC1155Yul");
     }
 
     function test_basic() public {
         assertEq(true, true);
+    }
+
+    function test_readOwner() public {
+        address addrInSlot0 = address(uint160(uint256(vm.load(address(token), bytes32(uint256(0))))));
+        assertEq(addrInSlot0, defaultSender, "Address in slot 0 does not match expected value");
+    }
+
+    function test_mint() public {
+        uint256 amount = 100;
+
+        vm.prank(defaultSender);
+        bytes memory mintData = abi.encodeWithSelector(0x156e29f6, user1, tokenId1, amount);
+        bytes memory balanceOfData = abi.encodeWithSelector(0x00fdd58e, user1, tokenId1);
+
+        (bool mint_success, ) = token.call(mintData);
+        (, bytes memory balanceOf_returndata) = token.call(balanceOfData);
+
+        assertEq(mint_success, true);
+        assertEq(abi.decode(balanceOf_returndata, (uint256)), amount, "Minting failed");
     }
 }
